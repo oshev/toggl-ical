6413af6ecf643164074a1b785ca9e3e2d07ce244
oleg-shevelev <oleg.shevelyov@schibsted.com>
2017-06-10 19:59:52 +0100

    - Creating iCal from Toggl JSON. - Saving iCal in a file which can be imported via Google Calendar UI.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # toggl-ical
 Tool to save Toggl time entries to iCal 
 
-DONE: Receiving Toggl items in JSON.
+DONE: 
+- Receiving Toggl items in JSON.
+- Creating iCal from Toggl JSON.
+- Saving iCal in a file which can be imported via Google Calendar UI.
 
 TODO: 
-- Creating iCal from Toggl JSON.
-- Saving iCal in a cloud with http serving to be able to add this file to Google Calendar and dynamically update it.
+- Let Google Calendar get this calendar automatically. 2 options:
+    - Save iCal file in a cloud with http serving.
+    - Create a HTTP service which re-creates the calendar each time Google calls the service. 
--- toggl_ical/toggl2ical.py
+++ toggl_ical/toggl2ical.py
@@ -3,9 +3,53 @@ import requests
 from datetime import datetime, timedelta
 import json
 import icalendar
+from dateutil.parser import parse
 
 TOGGL_URL_TEMPLATE = "https://www.toggl.com/api/v8/time_entries?start_date={}&end_date={}"
 TOGGL_TIME_FORMAT = "%Y-%m-%dT%H:%M:00Z"
+
+
+def get_entries(api_token, days):
+    end_date = datetime.now().strftime(TOGGL_TIME_FORMAT.format(TOGGL_TIME_FORMAT))
+    start_date = (datetime.now() - timedelta(days=int(days))).strftime(TOGGL_TIME_FORMAT.format(TOGGL_TIME_FORMAT))
+
+    response = requests.get(TOGGL_URL_TEMPLATE.format(start_date, end_date), auth=(api_token, 'api_token'))
+    if response.status_code == 200:
+        time_entries = json.loads(response.text)
+        print("Got {} time entries".format(len(time_entries)))
+        return time_entries
+    else:
+        print("Get error code from the API: {}".format(response.status_code))
+        return []
+
+
+def build_calendar(time_entries):
+    # Create the Calendar
+    calendar = icalendar.Calendar()
+    calendar.add('prodid', "toggl2ical")
+    calendar.add('version', '2.0')
+    calendar.add('CALSCALE', 'GREGORIAN')
+    calendar.add('X-WR-CALNAME', 'Toggl events')
+    calendar.add('method', 'publish')
+
+    for entry in time_entries:
+        event = icalendar.Event()
+        if all(field in entry for field in ["start", "description", "id"]):
+            event.add('summary', entry["description"])
+            if "tags" in entry:
+                event.add('description', ", ".join(entry["tags"]))
+            event.add('uid', entry["id"])
+            event.add('dtstart', parse(entry["start"]))
+            if "stop" in entry:
+                event.add('dtend', parse(entry["stop"]))
+            else:
+                event.add('dtend', datetime.now())
+            event.add('dtstamp', datetime.now())
+            # event.add('STATUS', 'CANCELLED')
+            calendar.add_component(event)
+
+    ical = str(calendar.to_ical()).replace("\\r\\n", "\n").strip("b'").strip("'")
+    return ical
 
 if __name__ == '__main__':
 
@@ -16,16 +60,11 @@ if __name__ == '__main__':
                         required=True, default=None)
 
     args = parser.parse_args()
-    api_token = args.token
-    days = args.days
 
-    end_date = datetime.now().strftime(TOGGL_TIME_FORMAT.format(TOGGL_TIME_FORMAT))
-    start_date = (datetime.now() - timedelta(days=int(days))).strftime(TOGGL_TIME_FORMAT.format(TOGGL_TIME_FORMAT))
+    entries = get_entries(args.token, args.days)
+    ical_text = build_calendar(entries)
+    with open("calendar.ics", "w") as file:
+        file.write(ical_text)
 
-    response = requests.get(TOGGL_URL_TEMPLATE.format(start_date, end_date), auth=(api_token, 'api_token'))
-    if response.status_code == 200:
-        time_entries = json.loads(response.text)
-        print("Got {} entries".format(len(time_entries)))
-    else:
-        print("Get error code from the API: {}".format(response.status_code))
+
 
